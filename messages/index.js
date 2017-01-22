6f21532e713cf96ce3285ebd04c4018ee2b36d5c
aagalati <galati_ator@msn.com>
2017-01-21 19:05:14 -0500

    fix movie intent

--- messages/index.js
+++ messages/index.js
@@ -42,7 +42,7 @@ var intents = new builder.IntentDialog({ recognizers: [recognizer] })
 .matches('movie', (session, args) => {
 	var genre = builder.EntityRecognizer.findEntity(args.entities, 'genre');
     if (genre != null) {
-		session.send('You asked me about %s movies', genre.entity);
+		session.send('You asked me about %s kind of movies', genre.entity);
     }
     else {
     	session.send('You asked me about movies');
